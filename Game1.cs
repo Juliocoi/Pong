ff2097d1b7402fd64d1ef86c782a8a5173635c8b
Juliocoi <amorim.jc@gmail.com>
2024-03-20 20:50:00 -0300

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pong
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Player _player1;
+        private Player _player2;
+        private Texture2D _barTexture;
+        private Texture2D _background;
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            base.Initialize();
+
+            _player1 = new Player(this, new Vector2(0.0f, 100), _barTexture, Keys.W, Keys.S);
+            _player2 = new Player(this, new Vector2(_graphics.PreferredBackBufferWidth - _barTexture.Width, 100), _barTexture, Keys.Up, Keys.Down);
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            _background = Content.Load<Texture2D>("assets/background");
+            _barTexture = Content.Load<Texture2D>("assets/bar");
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            
+            // TODO: Add your update logic here
+            _player1.Update();
+            _player2.Update();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(_background, Vector2.Zero, Color.White);
+            _player1.Draw(_spriteBatch);
+            _player2.Draw(_spriteBatch);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pong;
+
+public class Player
+{
+
+    private const float PLAYER_VELOCITY = 5F;
+    Vector2 _position;
+    Texture2D _texture;
+    Keys _keyUp;
+    Keys _keysDown;
+    Game _game; // declarado para usar a GraficsDevice e pegar a viewport
+
+    public Player(Game game, Vector2 position, Texture2D texture, Keys keyUp, Keys keyDown)
+    {
+        _game = game;
+        _position = position;
+        _texture = texture;
+        _keyUp = keyUp;
+        _keysDown = keyDown;
+    }
+
+    public void Update()
+    {
+        var keyboard = Keyboard.GetState();
+
+        if(keyboard.IsKeyDown(_keyUp))//IsKeyDown verifica se a tecla está precionada. IsKeyUP verifica se a tecla não está precionada.
+        {
+            _position.Y -= PLAYER_VELOCITY;
+        }
+        else if (keyboard.IsKeyDown(_keysDown))
+        {
+            _position.Y += PLAYER_VELOCITY;
+        }
+
+        // Colisão das barras com os limites verticais
+        var viewport = _game.GraphicsDevice.Viewport;
+
+        if(_position.Y < 0)
+        {
+            _position.Y = 0.0f;
+        }
+        else if(_position.Y + _texture.Height > viewport.Height)
+        {
+            _position.Y = viewport.Height - _texture.Height;
+        }
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(_texture, _position, Color.White);
+    }
+}
